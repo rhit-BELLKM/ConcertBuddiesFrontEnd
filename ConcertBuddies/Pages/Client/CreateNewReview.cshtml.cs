a5fe112735b7f5e997b336e0d30812a11dbe79f6
rhit-BELLKM <bellkm@rose-hulman.edu>
2022-11-09 22:45:16 -0500

    review start - changed layout and index

---
+++ ConcertBuddies/Pages/Client/CreateNewReview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConcertBuddies.Pages.Client
+{
+    public class CreateNewReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ConcertBuddies/Pages/Client/ReviewList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConcertBuddies.Pages.Client
+{
+    public class ReviewListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
