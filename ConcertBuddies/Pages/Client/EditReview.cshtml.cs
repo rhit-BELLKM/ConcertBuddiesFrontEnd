c3718ae84d9505ef9bb66bf49b248d4d00a7ab10
rhit-BELLKM <bellkm@rose-hulman.edu>
2022-11-09 23:41:22 -0500

    review stuff

--- ConcertBuddies/Pages/Client/EditReview.cshtml.cs
+++ ConcertBuddies/Pages/Client/EditReview.cshtml.cs
@@ -5,33 +5,30 @@ using System.Data.SqlClient;
 
 namespace ConcertBuddies.Pages.Client
 {
-    public class CreateNewReviewModel : PageModel
+    public class EditReviewModel : PageModel
     {
-        public ReviewInfo newReview = new ReviewInfo();
-        public String errorMessage = "";
-        public String successMessage = "";
+
+        public ReviewInfo review = new ReviewInfo();
+        String errorMessage = "";
+        String successMessage = "";
         public void OnGet()
         {
         }
 
         public void OnPost()
         {
-            int concertID = 1;
-            int concertgoerID = 90;
-            newReview.description = Request.Form["reviewDescription"];
-            newReview.rating = Convert.ToInt32(Request.Form["reviewRating"]);
-            if (newReview.description.Length == 0)
-            {
-                errorMessage = "All fields in the form are required.";
-                return;
-            }
+            int concertID = Int32.Parse(HttpContext.Request.Query["id"]);
+            int concertgoerID = Int32.Parse(HttpContext.Request.Query["concertgoerID"]);
+            String errorMessage = "";
+            String successMessage = "";
+
             try
             {
                 String connectionString = "Data Source=titan.csse.rose-hulman.edu;Initial Catalog=ConcertReviewSystem10;Persist Security Info=True;User ID=ConcertGroup;Password=UnluckyDucky_15";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    using (SqlCommand command = new SqlCommand("InsertReview", connection))
+                    using (SqlCommand command = new SqlCommand("UpdateReviewed", connection))
                     {
                         command.CommandType = System.Data.CommandType.StoredProcedure;
                         SqlParameter ConcertID = new SqlParameter
@@ -48,30 +45,16 @@ namespace ConcertBuddies.Pages.Client
                             SqlDbType = System.Data.SqlDbType.Int,
                             Direction = System.Data.ParameterDirection.Input
                         };
-                        SqlParameter Description = new SqlParameter
-                        {
-                            ParameterName = "@description",
-                            Value = newReview.description,
-                            SqlDbType = System.Data.SqlDbType.NVarChar,
-                            Direction = System.Data.ParameterDirection.Input
-                        };
-                        SqlParameter Rating = new SqlParameter
-                        {
-                            ParameterName = "@rating",
-                            Value = newReview.rating,
-                            SqlDbType = System.Data.SqlDbType.Int,
-                            Direction = System.Data.ParameterDirection.Input
-                        };
 
                         command.Parameters.Add(ConcertID);
                         command.Parameters.Add(ConcertgoerID);
-                        command.Parameters.Add(Description);
-                        command.Parameters.Add(Rating);
                         command.ExecuteNonQuery();
                     }
-                }
 
-            } catch (Exception e)
+
+                }
+            }
+            catch (Exception e)
             {
                 errorMessage = e.Message;
                 return;
--- ConcertBuddies/Pages/Client/ReviewList.cshtml.cs
+++ ConcertBuddies/Pages/Client/ReviewList.cshtml.cs
@@ -1,12 +1,68 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace ConcertBuddies.Pages.Client
 {
     public class ReviewListModel : PageModel
     {
+        public List<ReviewInfo> listReview = new List<ReviewInfo>();
         public void OnGet()
         {
+        
+            try
+            {
+                // Establishes the connection to the database
+                String connectionString = "Data Source=titan.csse.rose-hulman.edu;Initial Catalog=ConcertReviewSystem10;Persist Security Info=True;User ID=ConcertGroup;Password=UnluckyDucky_15";
+
+                // Creates connection
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("ReadTables", connection))
+                    {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlParameter Identifier = new SqlParameter
+                        {
+                            ParameterName = "@Identifier",
+                            Value = 7,
+                            SqlDbType = System.Data.SqlDbType.Int,
+                            Direction = System.Data.ParameterDirection.Input
+                        };
+
+                        command.Parameters.Add(Identifier);
+                        command.ExecuteNonQuery();
+
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            ReviewInfo review = new ReviewInfo();
+                            review.concertID = 1;
+                            review.concertgoerID = 90;
+                            review.description = reader["description"].ToString();
+                            review.rating = Convert.ToInt32(reader["rating"]);
+
+
+                            listReview.Add(review);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return;
+            }
         }
     }
+    public class ReviewInfo
+    {
+        public int concertID;
+        public int concertgoerID;
+        public String description;
+        public int rating;
+    }
+
 }
